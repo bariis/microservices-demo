316633a693c4f74b63a7332039f36d58878b9362
Barış Ertaş <ertasbariis@gmail.com>
2021-04-25 00:47:01 +0300

    removed prev. db

--- productservice/server.js
+++ productservice/server.js
@@ -1,21 +1,14 @@
 const express = require('express');
 const morgan = require('express');
 
-const db = require('./db');
-
-const PORT = process.env.PORT || 5000;
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 
 app.get('/', (req, res) => res.send('hello world'));
-
-app.get('/users', async (req, res) => {
-    const users = await db.select().from('users');
-    res.json(users);
-});
 
 app.listen(PORT, () => {
     console.log(`Server is up at http://localhost:${PORT}`)
