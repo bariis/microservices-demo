fb10d96a14794ca65273207ce77a0d19b275d5c8
Barış Ertaş <ertasbariis@gmail.com>
2021-04-25 17:21:46 +0300

    refactoring

--- productservice/sql/create-table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS product (
-    product_id INT NOT NULL,
-    product_name varchar(20) NOT NULL,
-    product_description varchar(100) NOT NULL,
-    product_price FLOAT NOT NULL,
-    PRIMARY KEY (product_id)
-);
