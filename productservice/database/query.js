43c664f4ef5c8395800842a75eddfc4e945e581b
Barış Ertaş <ertasbariis@gmail.com>
2021-04-25 17:14:35 +0300

    refactoring

--- productservice/database/query.js
+++
@@ -1,9 +0,0 @@
-const query = `
-CREATE TABLE product (
-    product_id VARCHAR(10),
-    productName VARCHAR(10),
-    productDescription VARCHAR(10),
-    productPrice INT);
-`;
-
-module.exports = query;
