9db54b59a4d4aaaf4138bde21640c84f1d8cf367
Barış Ertaş <ertasbariis@gmail.com>
2021-04-25 17:15:18 +0300

    add filling table script

---
+++ productservice/sql/fill-table.sql
@@ -0,0 +1,17 @@
+INSERT INTO product (product_id, product_name, product_description, product_price)
+VALUES
+     (1001, 'Product 1', 'A product', 12.99),
+     (1002, 'Product 2', 'A product', 13.99),
+     (1003, 'Product 3', 'A product', 14.99),
+     (1004, 'Product 4', 'A product', 15.99),
+     (1005, 'Product 5', 'A product', 16.99),
+     (1006, 'Product 6', 'A product', 18.99),
+     (1007, 'Product 7', 'A product', 19.99),
+     (1008, 'Product 8', 'A product', 20.99),
+     (1009, 'Product 9', 'A product', 21.99),
+     (1010, 'Product 1', 'A product', 22.99);
+
+
+
+
+
