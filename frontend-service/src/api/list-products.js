5fec34ffb74fdd70d78bd937ccf38fa51e5b5f27
Barış Ertaş <ertasbariis@gmail.com>
2021-05-09 18:21:33 +0300

    delete react files

--- frontend-service/src/api/list-products.js
+++
@@ -1,6 +0,0 @@
-export async function getProducts() {
-    const response = await fetch('http://localhost:5000/api/products');
-    const products = await response.json();
-    console.log(products);
-    return products;
-}
