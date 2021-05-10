58ec9a5ca519b5e3b4318764c2ecfde417935221
Barış Ertaş <ertasbariis@gmail.com>
2021-05-10 19:19:00 +0300

    add get product handler

--- productservice/server.js
+++ productservice/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const morgan = require('express');
 const cors = require('cors');
-const getProducts = require('./api');
+const api = require('./api');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -10,7 +10,9 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 app.use(cors());
-app.get('/api/products', getProducts);
+
+app.get('/api/products', api.getProducts);
+app.get('/api/products/:id', api.getProductById);
 
 app.listen(PORT, () => {
     console.log(`Server is up at http://localhost:${PORT}`);
