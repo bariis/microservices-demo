020378a56f478a4adc7402d9b3d8dfbd33d24cd8
Barış Ertaş <ertasbariis@gmail.com>
2021-05-10 19:17:15 +0300

    add getPruduct api

--- productservice/api.js
+++ productservice/api.js
@@ -13,4 +13,20 @@ async function getProducts(req, res) {
     } 
 }
 
-module.exports = getProducts;
+async function getProductById(req, res) {
+    let productId = parseInt(req.params.id); 
+    try {
+        const client = await pool.connect();
+        const product = await client.query(`SELECT * FROM product WHERE product_id = ${productId}`);
+        client.release();
+        res.status(200).json(product.rows);
+    } catch(err) {
+        console.error(`Error fetching single product: ${err}`);
+        res.status(500).send();
+    }
+}
+
+module.exports = {
+    getProducts,
+    getProductById
+}
