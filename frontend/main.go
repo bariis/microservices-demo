87fd8dc9c8b55862472715a1a22026afbffa19d0
Barış Ertaş <ertasbariis@gmail.com>
2021-05-16 23:42:51 +0300

    add mux server

--- frontend/main.go
+++ frontend/main.go
@@ -1,5 +1,11 @@
 package main
 
-func main() {
+import "github.com/gorilla/mux"
 
+func main() {
+	server := FrontendServer{
+		Router: mux.NewRouter(),
+	}
+	server.InitializeRoutes()
+	server.Run(":8080")
 }
