92254dace1dae2d1db3051ce9b8baaa501e8e8d8
Barış Ertaş <ertasbariis@gmail.com>
2021-07-16 23:56:25 +0300

    add client create query

--- sql/create-table.sql
+++ sql/create-table.sql
@@ -5,3 +5,13 @@ CREATE TABLE IF NOT EXISTS product (
     product_price FLOAT NOT NULL,
     PRIMARY KEY (product_id)
 );
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE client (
+    id uuid DEFAULT uuid_generate_v4 (),
+    full_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
