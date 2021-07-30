cc950c26d7a055aba579cb8176850dc659956cb7
Barış Ertaş <ertasbariis@gmail.com>
2021-07-31 02:32:23 +0300

    refactor main file

--- cartservice/main.go
+++ cartservice/main.go
@@ -1,12 +1,7 @@
 package main
 
-import "github.com/go-redis/redis/v8"
-
-func ExampleClient() {
-	rdb := redis.NewClient(&redis.Options{
-		Addr : "localhost:6379",
-		Password: "",
-		DB: 0,
-	}
-	err :=
+func main() {
+	s := server{}
+	s.Initialize()
+	s.Run()
 }
