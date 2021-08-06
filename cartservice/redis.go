4b359882e03126347929089a168520a65628c6d4
Barış Ertaş <ertasbariis@gmail.com>
2021-08-06 14:38:54 +0300

    add parse func for cartId

--- cartservice/redis.go
+++ cartservice/redis.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"errors"
+	"github.com/golang-jwt/jwt"
+)
+
 type product struct {
 	Id       string `json:"id"`
 	Name     string `json:"name"`
@@ -7,3 +12,13 @@ type product struct {
 	Quantity int    `json:"quantity"`
 }
 
+func parsePayload(token string) (string, error) {
+	parsedToken, _ := jwt.Parse(token, nil)
+	if parsedToken == nil {
+		return "", errors.New("error: empty token")
+	}
+
+	claims, _ := parsedToken.Claims.(jwt.MapClaims)
+
+	return claims["user_id"].(string), nil
+}
