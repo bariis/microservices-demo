2726fc11b860316f9238de22697b0d22854b0727
Barış Ertaş <ertasbariis@gmail.com>
2021-08-08 14:48:22 +0300

    remove unnecessary product and process token

--- cartservice/redis.go
+++ cartservice/redis.go
@@ -3,19 +3,16 @@ package main
 import (
 	"errors"
 	"github.com/golang-jwt/jwt"
+	"strings"
 )
 
-type product struct {
-	Id       string `json:"id"`
-	Name     string `json:"name"`
-	Price    string `json:"price"`
-	Quantity int    `json:"quantity"`
-}
-
-func parsePayload(token string) (string, error) {
-	parsedToken, _ := jwt.Parse(token, nil)
+// gets and returns the required part of the token
+func getUserIdFromToken(token string) (string, error) {
+	// remove the `bearer` part from the token
+	encodedToken := strings.Split(token, " ")[1]
+	parsedToken, _ := jwt.Parse(encodedToken, nil)
 	if parsedToken == nil {
-		return "", errors.New("error: empty token")
+		return "", errors.New("token is not valid")
 	}
 
 	claims, _ := parsedToken.Claims.(jwt.MapClaims)
