c5dbb4f329e3004bbad10c4ab57efb9f417e4a1a
Barış Ertaş <ertasbariis@gmail.com>
2021-08-23 23:06:51 +0300

    use product of hashes in set for cart

--- cartservice/redis.go
+++ cartservice/redis.go
@@ -26,9 +26,8 @@ func getUserIdFromToken(token string) (string, error) {
 }
 
 // add or updates the cart
-func AddItem(redis *redis.Client, productId string, productQuantity int, token string) error {
+func AddItem(redis *redis.Client, productName, productId string, productQuantity int, productPrice float64, token string) error {
 	ctx := context.Background()
-
 	// user_id will be stored as cartId like key in redis cache
 	cartId, err := getUserIdFromToken(token)
 	if err != nil {
@@ -36,27 +35,32 @@ func AddItem(redis *redis.Client, productId string, productQuantity int, token s
 	}
 
 	item := map[string]interface{}{
-		productId: productQuantity,
+		"id":       productId,
+		"name":     productName,
+		"quantity": productQuantity,
+		"price":    productPrice,
 	}
 
+	productIdentity := cartId + ":" + productId
 	cmd := redis.Exists(ctx, cartId)
 	if cmd.Val() == 0 {
-		redis.HSet(ctx, cartId, item)
+		redis.HSet(ctx, productIdentity, item)
+		redis.SAdd(ctx, cartId, cartId+":"+productId)
+		log.Printf("the cart with id %v created and the product with id %v added.\n", productId)
 	} else {
 		// If the item exists, we update its quantity
-		if cmd := redis.HExists(ctx, cartId, productId); cmd.Val() {
-			redis.HIncrBy(ctx, cartId, productId, int64(productQuantity))
-
-			// if the quantity of the item equals to zero, we delete this product from cache
-			if cmd := redis.HGet(ctx, cartId, productId); cmd.Val() == "0" {
-				redis.HDel(ctx, cartId, productId)
+		if cmd := redis.SIsMember(ctx, cartId, productIdentity); cmd.Val() {
+			// perform the operation on quantitiy. if current quantity of the product equals to zero we delete it from the cart
+			if currentQuantity := redis.HIncrBy(ctx, productIdentity, "quantity", int64(productQuantity)); currentQuantity.Val() == 0 {
+				log.Printf("the product with id %v has been updated\n", productId)
+				redis.SRem(ctx, cartId, productIdentity)
+				redis.Del(ctx, productIdentity)
 			}
 		} else {
-			redis.HSet(ctx, cartId, item)
+			redis.SAdd(ctx, cartId, productIdentity)
 		}
 	}
 
-	log.Printf("the product with id %v has been updated\n", productId)
 	// ttl for the current user's shopping cart
 	redis.Expire(ctx, cartId, 3*time.Hour)
 
@@ -85,7 +89,6 @@ func GetCart(redis *redis.Client, token string) (map[string]string, error) {
 	if err != nil {
 		return nil, fmt.Errorf("token: %v", err)
 	}
-
 	cmd := redis.HGetAll(ctx, cartId).Val()
 	log.Printf("the items of the card with id %v has been retrieved", cartId)
 
